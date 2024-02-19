c44f70e055918fa53a0ceca577201c85847a0cbf
Jason Nguyen <jasonnguyenphhs@gmail.com>
2024-02-19 13:04:33 -0800

    made changes so that backend can connect

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('backend is working');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
