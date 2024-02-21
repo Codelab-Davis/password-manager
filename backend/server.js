6bfd9615dd5dc1ad5f3829ac0cfa4dd7e227cbe2
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-02-20 19:55:47 -0800

    changed port number

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5002;
 
 app.use(express.json());
 app.use(cors());
