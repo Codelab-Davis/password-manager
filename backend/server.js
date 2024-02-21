2cd3029f71d17aca8b0e45011c7ae7ccea8b834e
simondgooden <sdgooden@ucdavis.edu>
2024-02-20 19:53:44 -0800

    test

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 
 app.use(express.json());
 app.use(cors());
