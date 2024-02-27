a094524c91793606bcf731adff9c8728589c1913
simondgooden <sdgooden@ucdavis.edu>
2024-02-26 19:21:48 -0800

    Made port 5000

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
