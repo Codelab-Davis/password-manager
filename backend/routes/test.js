092d19e4f876fe6b31d6e6253524e9749e3c4e8c
Suyash Goel <suyashgoel10@gmail.com>
2024-02-26 19:26:56 -0800

    Feature: get and post routes

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/test.model.js');
+const mongoose = require('mongoose');
 
+router.post("/", (req, res) => {
+    const { name, email, age, hobbies } = req.body;
 
-router.get('/', (req, res) => {
-    console.log("retrieved all users from the database")
-    User.find()
-        .then(users => res.json(users))
-        .catch(err => res.status(400).json('Error: ' + err));
+    if(!name || !email || !age || !hobbies) {
+        return res.status(400).json({error: "Not all attributes provided"});
+    }
+
+    const user = new User({ _id: new mongoose.Types.ObjectId(), name, email, age, hobbies });
+
+    user.save()
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(err => {
+      res.status(500).json({error: err});
+    });
+
+    return;
 });
 
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    User.findById(id)
+    .then(user => {
+        if(!user){
+            return res.status(400).json({error: "Item not found"});
+        }
+        return res.status(200).json(user);
+    })
+    .catch(err => {
+        return res.status(500).json({error: err});
+    });
 
+    return;
+});
 
 module.exports = router;
