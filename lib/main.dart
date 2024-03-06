d973ae388ecf6a2c4731e06bbbc0a1605e85c929
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-03-06 02:58:40 -0800

    Add separate page for displaying generated TOTP

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,14 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:otp/otp.dart';
 import 'package:timezone/data/latest.dart' as timezone;
 import 'package:clipboard/clipboard.dart';
-import 'package:http/http.dart' as http;
 import 'package:timezone/timezone.dart' as timezone;
 import 'package:logger/logger.dart';
-
-
-String code = "";
 
 void main() {
   runApp(MyApp());
   timezone.initializeTimeZones();
-  generateOTP();
 }
-
-void generateOTP() async {
-  while (true) {
-    final now = DateTime.now();
-
-    final pacificTimeZone = timezone.getLocation('America/Los_Angeles');
-
-    final date = timezone.TZDateTime.from(now, pacificTimeZone);
-
-    code = OTP.generateTOTPCodeString('ISHANT', date.millisecondsSinceEpoch,
-        length: 6, interval: 5, algorithm: Algorithm.SHA256, isGoogle: true);
-
-    await Future.delayed(const Duration(seconds: 5));
-  }
-}
-
-
-final Logger _logger = Logger();
 
 class MyApp extends StatelessWidget {
   @override
@@ -48,16 +24,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
-
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String otp = code;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,46 +40,83 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FloatingActionButton(
+            ElevatedButton(
               onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => StatefulBuilder(
-                    builder: (BuildContext context, StateSetter setState) {
-                      return AlertDialog(
-                        title: const Text('Passcode'),
-                        content:
-                            Text(otp.substring(0, 3) + " " + otp.substring(3)),
-                        actions: [
-                          ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                otp = code;
-                              });
-                            },
-                            child: const Icon(Icons.refresh),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              FlutterClipboard.copy(otp);
-                            },
-                            child: const Text('Copy'),
-                          )
-                        ],
-                      );
-                    },
-                  ),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GenerateTOTPPage()),
                 );
               },
-              tooltip: 'Generate OTP pop-up',
-              child: const Text(
-                'TOTP Pop-up',
-                textAlign: TextAlign.center,
-              ),
+              child: Text('Generate TOTP'),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class GenerateTOTPPage extends StatefulWidget {
+  @override
+  _GenerateTOTPPageState createState() => _GenerateTOTPPageState();
+}
+
+class _GenerateTOTPPageState extends State<GenerateTOTPPage> {
+  late String otp;
+
+  @override
+  void initState() {
+    super.initState();
+    generateOTP();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TOTP Generator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Generated TOTP:',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 10),
+            Text(
+              otp.substring(0, 3) + " " + otp.substring(3),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  generateOTP();
+                });
+              },
+              child: Text('Reload'),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                FlutterClipboard.copy(otp);
+              },
+              child: Text('Copy'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void generateOTP() {
+    final now = DateTime.now();
+    final pacificTimeZone = timezone.getLocation('America/Los_Angeles');
+    final date = timezone.TZDateTime.from(now, pacificTimeZone);
+
+    otp = OTP.generateTOTPCodeString('ISHANT', date.millisecondsSinceEpoch,
+        length: 6, interval: 5, algorithm: Algorithm.SHA256, isGoogle: true);
   }
 }
