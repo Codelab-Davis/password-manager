b332d412b5861bdd49f645ab025cd5e57bcca776
jason3N <jasonnguyenphhs@gmail.com>
2024-03-26 22:44:18 -0700

    aaa

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,94 +1,166 @@
+import 'dart:async';
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart'; // For clipboard operations
 import 'package:otp/otp.dart';
+import 'package:timezone/data/latest.dart' as timezone;
 import 'package:timezone/timezone.dart' as timezone;
 
+void main() {
+  runApp(const MyApp());
+  timezone.initializeTimeZones();
+}
 
-String code = "";
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter OTP Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'TOTP Display'),
+    );
+  }
+}
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String otp = code;
-
-  @override
-  void initState() {
-    super.initState();
-    generateOTP();
-  }
-  
-void generateOTP() async {
-  while (true) { // Ensure loop continues only while widget is mounted
-    final now = DateTime.now();
-    final pacificTimeZone = timezone.getLocation('America/Los_Angeles');
-    final date = timezone.TZDateTime.from(now, pacificTimeZone);
-
-    final newCode = OTP.generateTOTPCodeString('ISHANT', date.millisecondsSinceEpoch,
-        length: 6, interval: 1, algorithm: Algorithm.SHA256, isGoogle: true);
-
-    setState(() {
-      code = newCode; // Update the global 'code' variable
-      otp = code; // Ensure 'otp' is updated with the new code, triggering a widget update
-    });
-
-    await Future.delayed(const Duration(seconds: 1)); // Wait for the next interval before updating
-  }
-}
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FloatingActionButton(
+            ElevatedButton(
               onPressed: () {
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('Passcode'),
-                    content: Text(otp.substring(0, 3) + " " + otp.substring(3)),
-                    actions: [
-                      ElevatedButton(
-                        onPressed: () {
-                          otp = code;
-                        },
-                        child: const Icon(Icons.refresh),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          Clipboard.setData(ClipboardData(text: otp));
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(content: Text('OTP copied to clipboard')),
-                          );
-                        },
-                        child: const Text('Copy'),
-                      ),
-                    ],
-                  ),
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GenerateTOTPPage()),
                 );
               },
-              tooltip: 'Generate OTP',
-              child: const Text(
-                'Generate OTP',
-                textAlign: TextAlign.center,
-              ),
+              child: const Text('Generate TOTP'),
             ),
           ],
         ),
       ),
     );
+  }
+}
+
+class GenerateTOTPPage extends StatefulWidget {
+  @override
+  _GenerateTOTPPageState createState() => _GenerateTOTPPageState();
+}
+
+class _GenerateTOTPPageState extends State<GenerateTOTPPage> {
+  String otp = "";
+  int reloadTimer = 30; // Initial reload time in seconds for visual countdown
+  Timer? countdownTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    generateOTP();
+    startReloadTimer();
+  }
+
+  @override
+  void dispose() {
+    countdownTimer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TOTP Generator'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Generated TOTP:',
+              style: TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              otp.substring(0, 3) + " " + otp.substring(3),
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              'Reload in $reloadTimer seconds',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                generateOTP();
+                resetReloadTimer();
+              },
+              child: const Text('Reload'),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                FlutterClipboard.copy(otp).then((_) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('OTP copied to clipboard')),
+                  );
+                });
+              },
+              child: const Text('Copy'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void generateOTP() {
+    final now = DateTime.now();
+    final pacificTimeZone = timezone.getLocation('America/Los_Angeles');
+    final date = timezone.TZDateTime.from(now, pacificTimeZone);
+
+    setState(() {
+      otp = OTP.generateTOTPCodeString('ABCDEF', date.millisecondsSinceEpoch,
+          length: 6, interval: 30, algorithm: Algorithm.SHA256, isGoogle: true);
+    });
+  }
+
+  void startReloadTimer() {
+    countdownTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        if (reloadTimer > 0) {
+          reloadTimer--;
+        } else {
+          generateOTP(); // Automatically generate a new OTP
+          reloadTimer = 30; // Reset reload time
+        }
+      });
+    });
+  }
+
+  void resetReloadTimer() {
+    setState(() {
+      reloadTimer = 30; // Reset the reload timer to 30 seconds
+    });
   }
 }
