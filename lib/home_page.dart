497af81232a73dd896866126f69aa307e0c93a9f
jason3N <jasonnguyenphhs@gmail.com>
2024-03-30 20:23:13 -0700

    merged simons code into main

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -5,6 +5,10 @@ import 'package:flutter/services.dart'; // For clipboard operations
 import 'package:otp/otp.dart';
 import 'package:timezone/data/latest.dart' as timezone;
 import 'package:timezone/timezone.dart' as timezone;
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
 
 void main() {
   runApp(const MyApp());
@@ -30,13 +34,41 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+  
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+postData(String email, String password) async {
+  try {
+    var url = Uri.http('localhost:5000', '/test/add');
+    var response = await http.post(url,
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'email': email, 
+        'password': password,
+      }),
+    );
+    //print('Response status: ${response.statusCode}'); //Helpful for debugging
+    //print('Response body: ${response.body}'); //Helpful for debugging
+  } catch(e) {
+    print(e);
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  bool isHidden = true;
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  
+  
+
   @override
+  
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -46,6 +78,44 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+          Padding (
+            padding: const EdgeInsets.only(left: 200.0, right: 200.0), // Adds padding of 20 pixels to the left and right
+            child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(), 
+                  hintText: 'Enter Email:'
+                  ),
+                controller: emailController,
+            ),
+          ),
+            const SizedBox(height: 10),
+            Padding (
+            padding: const EdgeInsets.only(left: 200.0, right: 200.0), // Adds padding of 20 pixels to the left and right
+            child:
+            TextField(
+              obscureText: isHidden, // Use the isHidden variable here
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: 'Enter Password:',
+                suffixIcon: IconButton(
+                  onPressed: togglePassword,
+                  icon: Icon(
+                    isHidden ? Icons.visibility : Icons.visibility_off,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              controller: passwordController,
+            ),
+            ),
+            const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  postData(emailController.text, passwordController.text);   
+              },
+                child: const Text("Submit")
+            ),
+            const SizedBox(height: 150),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -60,6 +130,11 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+    void togglePassword() {
+    setState(() {
+      isHidden = !isHidden;
+    });
+  }
 }
 
 class GenerateTOTPPage extends StatefulWidget {
@@ -71,6 +146,12 @@ class _GenerateTOTPPageState extends State<GenerateTOTPPage> {
   String otp = "";
   int reloadTimer = 30; // Initial reload time in seconds for visual countdown
   Timer? countdownTimer;
+
+  bool isHidden = true;
+  
+  // TextEditingController _textController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 
   @override
   void initState() {
@@ -164,3 +245,5 @@ class _GenerateTOTPPageState extends State<GenerateTOTPPage> {
     });
   }
 }
+
+
