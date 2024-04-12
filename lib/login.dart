86933a4186ed39c074d1f761cfbcd617ce2b2d5f
Suyash Goel <suyashgoel10@gmail.com>
2024-04-12 15:32:26 -0700

    Feature: Horizontal divider, third-party sign in

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,8 +11,16 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool checked = false;
+  bool showPassword = true;
 
-  var checked = false;
+  Color getColor(Set<MaterialState> states) {
+    if (checked) {
+      return Colors.black;
+    } else {
+      return Colors.white;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -61,93 +72,130 @@ class _LoginState extends State<Login> {
                 const SizedBox(
                   height: 15,
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(
+                Padding(
+                  padding: const EdgeInsets.only(
                       left: 20.0,
                       right:
                           20.0), // Adds padding of 20 pixels to the left and right
                   child: TextField(
+                    obscureText: showPassword,
                     decoration: InputDecoration(
-                        border: OutlineInputBorder(), hintText: 'Password'),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-/*
-class Login extends StatelessWidget {
-  const Login({super.key});
-
-  @override
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  width: 179,
-                  height: 177,
-                  decoration: ShapeDecoration(
-                    color: const Color(0xFFD9D9D9),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(31),
+                      border: OutlineInputBorder(),
+                      hintText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(showPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(
+                            () {
+                              showPassword = !showPassword;
+                            },
+                          );
+                        },
+                      ),
                     ),
                   ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const SizedBox(width: 5),
+                        Checkbox(
+                          fillColor:
+                              MaterialStateProperty.resolveWith(getColor),
+                          checkColor: Colors.white,
+                          value: checked,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              checked = value!;
+                            });
+                          },
+                        ),
+                        const Text(
+                          'Remember Me',
+                          style: TextStyle(
+                            color: Color(0xFF323232),
+                            fontSize: 15,
+                            fontFamily: 'Outfit',
+                            fontWeight: FontWeight.w400,
+                            height: 0.06,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Row(
+                      children: [
+                        Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Color(0xFF323232),
+                            fontSize: 15,
+                            fontFamily: 'Outfit',
+                            fontWeight: FontWeight.bold,
+                            height: 0.06,
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                      ],
+                    ),
+                  ],
                 ),
                 const SizedBox(
                   height: 20,
                 ),
-                const Text(
-                  'Welcome!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Color(0xFF313131),
-                    fontSize: 40,
-                    fontFamily: 'Outfit',
-                    fontWeight: FontWeight.w700,
-                    height: 0,
-                  ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 20.0, right: 15.0),
+                        child: const Divider(),
+                      ),
+                    ),
+                    const Text("or"),
+                    Expanded(
+                      child: Container(
+                        margin: const EdgeInsets.only(left: 10.0, right: 15.0),
+                        child: const Divider(),
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(
-                  height: 15,
+                  height: 25,
                 ),
-                const Padding(
-                  padding: EdgeInsets.only(
-                      left: 20.0,
-                      right:
-                          20.0), // Adds padding of 20 pixels to the left and right
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), hintText: 'Email'),
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.apple),
+                      iconSize: 50,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.wine_bar),
+                      iconSize: 50,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.facebook),
+                      iconSize: 50,
+                      onPressed: () {},
+                    )
+                  ],
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                const Padding(
-                  padding: EdgeInsets.only(
-                      left: 20.0,
-                      right:
-                          20.0), // Adds padding of 20 pixels to the left and right
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), hintText: 'Password'),
-                  ),
-                ),
-                Row(children: [
-                  Checkbox(value: checked, onChanged: )
-                ],),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Don't have an account?"),
+                    TextButton(
+                      child: const Text('Sign up'),
+                      onPressed: () {},
+                    )
+                  ],
+                )
               ],
             ),
           ),
@@ -156,5 +204,3 @@ class Login extends StatelessWidget {
     );
   }
 }
-
-*/
