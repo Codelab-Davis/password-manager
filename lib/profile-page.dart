41ac9982445629da34ae00704a872d1173e57952
ishantgandhi <126364217+ikenakami@users.noreply.github.com>
2024-04-23 17:37:44 -0700

    improved nav bar

--- lib/profile-page.dart
+++ lib/profile-page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:password_manager/global.dart';
 import 'package:password_manager/qrscanner-page.dart';
 import 'package:password_manager/totp-page.dart';
-import 'package:password_manager/passbook-page.dart';  
+import 'package:password_manager/passbook-page.dart';
 
 class UserProfilePage extends StatelessWidget {
   const UserProfilePage({Key? key}) : super(key: key);
@@ -11,44 +11,52 @@ class UserProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+      title: const Text('Profile'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'User Profile Page',
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const <Widget>[
+        Text(
+          'User Profile Page',
+          style: TextStyle(fontSize: 24),
         ),
+        ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(), // Shape for floating action button notch
+        notchMargin: 5,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(100), // Rounded corners
+          child: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 20), // Decrease the size of the icon
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.qr_code),
+            icon: Icon(Icons.qr_code, size: 20), // Decrease the size of the icon
             label: 'Scanner',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: Icon(Icons.person, size: 20), // Decrease the size of the icon
             label: 'Profile',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.settings), // Corrected icon for the fourth option
-            label: 'Settings', // Label for the new fourth button
+            icon: Icon(Icons.settings, size: 20), // Decrease the size of the icon
+            label: 'Settings',
           ),
         ],
         currentIndex: 2,
-        selectedItemColor: Color.fromARGB(255, 112, 175, 238),
-        unselectedItemColor: Colors.grey, 
+        selectedItemColor: const Color.fromARGB(255, 112, 175, 238),
+        unselectedItemColor: Colors.grey,
         onTap: (index) => _onItemTapped(index, context),
+        elevation: 8, // Elevation for the shadow effect
+          ),
+        ),
       ),
-    );
+      );
   }
 }
 
@@ -57,7 +65,9 @@ void _onItemTapped(int index, BuildContext context) {
     case 0:
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => GenerateTOTPPage(secret: Global.result?.code ??'')),
+        MaterialPageRoute(
+            builder: (context) =>
+                GenerateTOTPPage(secret: Global.result?.code ?? '')),
       );
       break;
     case 1:
@@ -69,11 +79,12 @@ void _onItemTapped(int index, BuildContext context) {
     case 2:
       break;
     case 3:
-        Navigator.pushReplacement(
-          context as BuildContext,
-            MaterialPageRoute(builder: (context) => const AccountsPage(),        
-              ),
-            );
+      Navigator.pushReplacement(
+        context as BuildContext,
+        MaterialPageRoute(
+          builder: (context) => const AccountsPage(),
+        ),
+      );
 
       break;
   }
